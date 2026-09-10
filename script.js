47d24541b8db9bdac1a7481cf235c1ac253b42a4
Josh14 <joshua.huizer@gmail.com>
2026-09-10 09:42:05 +0200

    Added functionality to the tab buttons, added css code for each section

---
+++ script.js
@@ -0,0 +1,13 @@
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        const targetId = link.getAttribute('href');
+        if (targetId === '#home') {
+            document.title = "Home - PromptCode";
+        } else if (targetId === '#about') {
+            document.title = "About - PromptCode";
+        } else if (targetId === '#form') {
+            document.title = "Request code writing/enhancing/debugging";
+        }
+    });
